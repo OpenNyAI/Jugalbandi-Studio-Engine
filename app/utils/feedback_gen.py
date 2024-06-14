3425a6d2a9fc5f0696a815817b6a99cbda6079b9
kanakraj <t-kanakraj@microsoft.com>
2024-06-14 17:07:45 +0530

    corrected the imports, modified the llm to use Azure Keys

--- app/utils/feedback_gen.py
+++ app/utils/feedback_gen.py
@@ -1,12 +1,13 @@
-from llm import llm
+from .llm import llm, sm, um
+
 
 def generate_feedback(user_history, dsl_desc):
     if not user_history or len(user_history) < 1:
-        return ''
+        return ""
 
     if not dsl_desc or len(dsl_desc) < 1:
-        return ''
-  
+        return ""
+
     template_str = """You are a developer bot that is developing a program as per the following user instruction : 
 {0}
 
@@ -17,13 +18,17 @@ Now think as a developer and list out ideas to improve the program. Restrict to 
 Ignore handling corner cases. Ignore validating variables. If there are no issues, dont report anything.
 Return at most 2 bullet points with at most 10 words each. Think step by step. Be clear and concise. Dont repeat similar ideas."""
 
-    user_instruction = ''
+    user_instruction = ""
     for i, inst in enumerate(user_history):
-        user_instruction += f'{i}: {inst}\n'
+        user_instruction += f"{i}: {inst}\n"
 
-    result = llm([
-        sm("You are a developer bot that is helping users improve a program by providing helpful suggestions"),
-        um(template_str.format(user_instruction, dsl_desc))
-    ])
+    result = llm(
+        [
+            sm(
+                "You are a developer bot that is helping users improve a program by providing helpful suggestions"
+            ),
+            um(template_str.format(user_instruction, dsl_desc)),
+        ]
+    )
 
     return result
--- app/utils/llm.py
+++ app/utils/llm.py
@@ -1,6 +1,6 @@
 import json
 import os
-import openai
+from openai import OpenAI, AzureOpenAI
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 from termcolor import colored
 
@@ -10,100 +10,132 @@ from termcolor import colored
 #     max=120),
 #     stop=stop_after_attempt(1))
 def llm(messages, **kwargs):
-    client = openai.Client(
-        api_key=os.environ["OPENAI_API_KEY"],
-    )
+    if os.getenv("AZURE_OPENAI_API_KEY"):
+        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+        azure_key = os.getenv("AZURE_OPENAI_API_KEY")
+        api_version = os.getenv("AZURE_OPENAI_API_VERSION")
+        client = AzureOpenAI(
+            azure_endpoint=azure_endpoint, api_key=azure_key, api_version=api_version
+        )
+    else:
+        client = OpenAI()
 
-    kwargs['model'] = kwargs.get('model', 'gpt-4-1106-preview')
-    kwargs['messages'] = messages
-    args = {k: v for k, v in kwargs.items() if k in [
-            'model', 'messages', 'temperature', 'tools', 'stream']}
+    kwargs["model"] = kwargs.get("model", "gpt-4-turbo")
+    kwargs["messages"] = messages
+    args = {
+        k: v
+        for k, v in kwargs.items()
+        if k in ["model", "messages", "temperature", "tools", "stream"]
+    }
 
-    if kwargs.get('debug', False):
-        others = {k: v for k, v in args.items() if k not in [
-            'messages', 'callback', 'debug']}
-        print(colored(others, 'yellow'))
-        for message in args['messages']:
-            print(colored(message['role'], 'green'))
-            print(colored(message['content'], 'yellow'))
+    if kwargs.get("debug", False):
+        others = {
+            k: v for k, v in args.items() if k not in ["messages", "callback", "debug"]
+        }
+        print(colored(others, "yellow"))
+        for message in args["messages"]:
+            print(colored(message["role"], "green"))
+            print(colored(message["content"], "yellow"))
 
-    if args.get('temperature', None) is None:
-        args['temperature'] = 1e-6
+    if args.get("temperature", None) is None:
+        args["temperature"] = 1e-6
 
-    completions = client.chat.completions.create(
-        **args
-    )
+    completions = client.chat.completions.create(**args)
 
-    if args.get('stream', False):
+    if args.get("stream", False):
         full_response = ""
         for chunk in completions:
             for choice in chunk.choices:
-                if choice.finish_reason == 'stop':
+                if choice.finish_reason == "stop":
                     break
                 if choice.delta.content is not None:
-                    kwargs['callback'](choice.delta.content)
+                    kwargs["callback"](choice.delta.content)
                     full_response += choice.delta.content
         return full_response
-    elif args.get('tools', None) is None:
+    elif args.get("tools", None) is None:
         return completions.choices[0].message.content
     else:
-        if kwargs.get('debug', False):
-            print(colored(completions.choices[0].message, 'yellow'))
+        if kwargs.get("debug", False):
+            print(colored(completions.choices[0].message, "yellow"))
         if completions.choices[0].message.tool_calls is None:
-            return {
-                'function': None,
-                'message': completions.choices[0].message.content
-            }
+            return {"function": None, "message": completions.choices[0].message.content}
         fn = completions.choices[0].message.tool_calls[0].function
-        return {
-            'function': fn.name,
-            'arguments': json.loads(fn.arguments)
-        }
+        return {"function": fn.name, "arguments": json.loads(fn.arguments)}
 
 
 def sm(prompt):
-    return {'role': 'system', 'content': prompt}
+    return {"role": "system", "content": prompt}
 
 
 def um(prompt):
-    return {'role': 'user', 'content': prompt}
+    return {"role": "user", "content": prompt}
 
 
 def am(prompt):
-    return {'role': 'assistant', 'content': prompt}
+    return {"role": "assistant", "content": prompt}
 
 
 def fn(name, description, params, required):
     return {
-        'type': 'function',
-        'function': {
-                'name': name,
-            'description': description,
-            'parameters': {
-                'type': 'object',
-                        'properties': {k: v for k, v in params.items()},
-                        'required': required
-            }
-        }
+        "type": "function",
+        "function": {
+            "name": name,
+            "description": description,
+            "parameters": {
+                "type": "object",
+                "properties": {k: v for k, v in params.items()},
+                "required": required,
+            },
+        },
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from dotenv import load_dotenv
+
     load_dotenv()
 
-    result = llm([
-        sm("Respond by using functions only"),
-        um("The story needs to have a good ending")
-    ], tools=[
-        fn('update_story', 'Update the current story', {'change': {
-            'type': 'string', 'description': 'instructions from the user'}}, ['change']),
-        fn('update_characters', 'Update the characters in the story', {'change': {
-            'type': 'string', 'description': 'instructions from the user'}}, ['change']),
-        fn('update_scenes', 'Update the scenes in the story', {'scene_number': {
-            'type': 'integer', 'description': 'scene number'},
-            'change': {
-            'type': 'string', 'description': 'instructions from the user'}}, ['change']),
-    ])
+    result = llm(
+        [
+            sm("Respond by using functions only"),
+            um("The story needs to have a good ending"),
+        ],
+        tools=[
+            fn(
+                "update_story",
+                "Update the current story",
+                {
+                    "change": {
+                        "type": "string",
+                        "description": "instructions from the user",
+                    }
+                },
+                ["change"],
+            ),
+            fn(
+                "update_characters",
+                "Update the characters in the story",
+                {
+                    "change": {
+                        "type": "string",
+                        "description": "instructions from the user",
+                    }
+                },
+                ["change"],
+            ),
+            fn(
+                "update_scenes",
+                "Update the scenes in the story",
+                {
+                    "scene_number": {"type": "integer", "description": "scene number"},
+                    "change": {
+                        "type": "string",
+                        "description": "instructions from the user",
+                    },
+                },
+                ["change"],
+            ),
+        ],
+    )
 
     print(result)
