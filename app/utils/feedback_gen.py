d107e3507083596a1c08ff7bf33306aadd0dee28
Vageesh <vageesh.dc@gmail.com>
2024-06-14 18:27:08 +0530

    use error in feedback

--- app/utils/feedback_gen.py
+++ app/utils/feedback_gen.py
@@ -1,18 +1,25 @@
 from .llm import llm, sm, um
 
 
-def generate_feedback(user_history, dsl_desc):
+def generate_feedback(user_history, dsl_desc, errors):
     if not user_history or len(user_history) < 1:
         return ""
 
     if not dsl_desc or len(dsl_desc) < 1:
         return ""
 
+    error_string = ""
+    if errors and len(errors) > 0:
+        error_string += "Logical errors found in the program plan :"
+        for e in errors:
+            error_string += f"\n- {e}"
+    
     template_str = """You are a developer bot that is developing a program as per the following user instruction : 
 {0}
 
 Here is the plan of how the program should be written with various tasks listed out:
 {1}
+{3}
 
 Now think as a developer and list out ideas to improve the program. Restrict to ideas that make the program logically complete or that add the important functionality. 
 Ignore handling corner cases. Ignore validating variables. If there are no issues, dont report anything.
@@ -27,7 +34,7 @@ Return at most 2 bullet points with at most 10 words each. Think step by step. B
             sm(
                 "You are a developer bot that is helping users improve a program by providing helpful suggestions"
             ),
-            um(template_str.format(user_instruction, dsl_desc)),
+            um(template_str.format(user_instruction, dsl_desc, error_string)),
         ]
     )
 
