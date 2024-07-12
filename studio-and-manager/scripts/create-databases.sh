2af47e929b7c590325b4311e2fcbf788f9e0fe62
kanak8278 <kanak8278@gmail.com>
2024-07-12 12:00:19 +0530

    dump files not mounting into docker init. removed the bug of creating tables in the postgres db

--- studio-and-manager/scripts/create-databases.sh
+++ studio-and-manager/scripts/create-databases.sh
@@ -16,11 +16,11 @@ EOSQL
 echo "jbmanager database created"
 
 # Import jbstudio_dump.sql into jbstudio database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname jbstudio -f /docker-entrypoint-initdb.d/jbstudio_dump.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname jbstudio -f /mnt/init/jbstudio_dump.sql
 
 echo "jbstudio_dump.sql imported into jbstudio database"
 
 # Import jbmanager_dump.sql into jbmanager database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname jbmanager -f /docker-entrypoint-initdb.d/jbmanager_dump.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname jbmanager -f /mnt/init/jbmanager_dump.sql
 
 echo "jbmanager_dump.sql imported into jbmanager database"
